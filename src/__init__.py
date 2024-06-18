93b0d435b0f846db5a32247e23f062c0f4c87210
matthewmerris <matthewmerris@u.boisestate.edu>
2024-06-18 16:03:01 -0600

    loewnerize activations function

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+import loewnerize_acts
+
+if __name__ == '__main__':
+    pass
---
+++ src/loewnerize_acts.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+import matlab.engine
+
+def loewnerize_acts(inputs_raw, outputs_raw, targets, tensorlab_path):
+    """Construct a loewner tensor out of layer activations
+    Parameters
+    __________
+    inputs_raw : ndarray
+    outputs_raw : ndarray
+
+    Returns
+    __________
+    loewner_x : ndarray
+    """
+
+    # reshape inputs and outputs to matrices
+    inputs = inputs_raw.reshape(-1, inputs_raw.shape[-1])
+    outputs = outputs_raw.reshape(-1, outputs_raw.shape[-1])
+
+    # instantiate empty containers for splitting activations according to target
+    i_targets = list()
+    o_targets = list()
+    for i in range(0,10):
+        i_targets.append(np.empty((0,inputs.shape[1])))
+        o_targets.append(np.empty((0,outputs.shape[1])))
+
+    # split activations according to targets
+    for idx, x in enumerate(targets):
+        o_targets[x] = np.vstack([o_targets[x], outputs[idx, :]])
+        i_targets[x] = np.vstack([i_targets[x], inputs[idx, :]])
+
+    # Loewnerize activations
+    eng = matlab.engine.start_matlab()
+    s = eng.genpath(tensorlab_path)
+    eng.addpath(s, nargout=0)
+
+    I = math.ceil(inputs.shape[1]/2)
+    J = inputs.shape[1] - I
+
+    loewner_tens = list()
+    for j in range(0,10):
+        num_rows = o_targets[j].shape[0]
+        loewner_tens.append(np.empty((I, J, 0)))
+        for i in range(0,num_rows):
+            loewner_tens[j] = np.dstack((loewner_tens[0], eng.loewnerize(o_targets[j][i, :], 'T', i_targets[j][i, :])))
+
+    return loewner_tens
+
+
+
+
