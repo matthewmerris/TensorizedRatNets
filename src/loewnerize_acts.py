beb1500d2536fb874f9ea79b157ee7ecea9e3387
matthewmerris <matthewmerris@u.boisestate.edu>
2024-06-19 10:15:00 -0600

    refinining lwn_acts

--- src/loewnerize_acts.py
+++ src/loewnerize_acts.py
@@ -38,12 +38,14 @@ def loewnerize_acts(inputs_raw, outputs_raw, targets, tensorlab_path):
     I = math.ceil(inputs.shape[1]/2)
     J = inputs.shape[1] - I
 
-    loewner_tens = list()
+    loewner_tens = np.empty(10)
+    # loewner_tens = list()
     for j in range(0,10):
         num_rows = o_targets[j].shape[0]
-        loewner_tens.append(np.empty((I, J, 0)))
+        loewner_tens[j] = np.empty((I, J, 0), dtype=Common_DataType)
+        # loewner_tens.append(np.empty((I, J, 0)))
         for i in range(0,num_rows):
-            loewner_tens[j] = np.dstack((loewner_tens[0], eng.loewnerize(o_targets[j][i, :], 'T', i_targets[j][i, :])))
+            loewner_tens[j] = np.dstack((loewner_tens[0], eng.loewnerize(o_targets[j][i, :], 'T', i_targets[j][i, :], nargout=1)))
 
     return loewner_tens
 
